4145833287f71bcb37dc32f5cb11a13d9ba5a40f
ahmedhany5 <faresahmed51857@gmail.com>
2024-03-17 01:52:22 +0200

    fixed supabase error

--- src/api/supabase.ts
+++ src/api/supabase.ts
@@ -1,5 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-    "https://fjzqjvqfjzqjvqfjzqfj.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN4a3Z6d2hwanZoeHhuamFxcmlyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDUyMzY2MDMsImV4cCI6MjAyMDgxMjYwM30.L6QK-0LD6emMt957LHP4PEB8vIdoRiQakPub_GjjH34"
+    "https://sxkvzwhpjvhxxnjaqrir.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN4a3Z6d2hwanZoeHhuamFxcmlyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDUyMzY2MDMsImV4cCI6MjAyMDgxMjYwM30.L6QK-0LD6emMt957LHP4PEB8vIdoRiQakPub_GjjH34"
 );
+
+
