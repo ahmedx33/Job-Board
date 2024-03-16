4b3927a49560b3ada6a682171ed5545b07f9ebe6
ahmedhany5 <faresahmed51857@gmail.com>
2024-03-17 01:50:37 +0200

    fixed supabase error

--- src/api/supabase.ts
+++ src/api/supabase.ts
@@ -1,6 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  import.meta.env.VITE_REACT_URL,
-  import.meta.env.VITE_REACT_KEY
+    "https://fjzqjvqfjzqjvqfjzqfj.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN4a3Z6d2hwanZoeHhuamFxcmlyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDUyMzY2MDMsImV4cCI6MjAyMDgxMjYwM30.L6QK-0LD6emMt957LHP4PEB8vIdoRiQakPub_GjjH34"
 );
