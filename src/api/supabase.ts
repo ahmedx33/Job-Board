8cf119d53671ca6b4258d010609db57a7232be40
ahmedhany5 <faresahmed51857@gmail.com>
2024-03-17 15:40:02 +0200

    added vercel.json

--- src/api/supabase.ts
+++ src/api/supabase.ts
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-    "https://sxkvzwhpjvhxxnjaqrir.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN4a3Z6d2hwanZoeHhuamFxcmlyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDUyMzY2MDMsImV4cCI6MjAyMDgxMjYwM30.L6QK-0LD6emMt957LHP4PEB8vIdoRiQakPub_GjjH34"
+  import.meta.env.VITE_REACT_URL,
+  import.meta.env.VITE_REACT_KEY
 );
-
-
