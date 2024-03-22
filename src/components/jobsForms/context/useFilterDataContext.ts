4b5fc7470ae6b4b796e059065bb6db5eb8c5444c
Ahmed Hany <133010783+ahmedx33@users.noreply.github.com>
2024-03-22 02:55:35 +0200

    Update useFilterDataContext.ts

--- src/components/jobsForms/context/useFilterDataContext.ts
+++ src/components/jobsForms/context/useFilterDataContext.ts
@@ -3,6 +3,6 @@ import { useContext } from 'react'
 import { FilterListingsContext } from "../pages/FilterListingsForm.tsx";
 export default function useFilterData() {
     const data = useContext(FilterListingsContext);
-
+    if (data === null) throw new Error("Somthing went wrong!")
     return data
 }
